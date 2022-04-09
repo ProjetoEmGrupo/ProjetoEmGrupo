432bae6f5eb86ad23e2e6791d9102635d29ed5b0
Jessica <jessicagprandini@gmail.com>
2022-04-09 20:02:22 -0300

    Corrige texto classe cliente

--- cineGenFlix/Cliente.java
+++ cineGenFlix/Cliente.java
@@ -9,13 +9,13 @@ public class Cliente  extends Pessoa{
 	
 	public void Cliente ()
 	{
-		System.out.println("Se você quer virar um cliente Super Mega Platinum, nos procure e cadastre o seu cartão!!\n");
+		System.out.println("Se voce quer virar um cliente Super Mega Platinum, nos procure e cadastre o seu cartÃ£o!!\n");
 		System.out.println();
 	}
 	
 	public void Cliente (String cartao)
 	{
-		System.out.println("\nParabéns você é um cliente Super Mega Platinum e pode ver o filme que quiser!!! *-*\n");
+		System.out.println("\nParabens voce Ã© um cliente Super Mega Platinum e pode ver o filme que quiser!!! *-*\n");
 	
 	}
 	
